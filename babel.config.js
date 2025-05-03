b1cd4d3268e41b9b1706a510cd703486a3268caf
shafarizkyf <shafarizkyf@gmail.com>
2025-05-03 20:58:28 +0700

    update tsconfig for aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src', './assets'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@images': './assets/images',
+          '@lottie': './assets/lottie',
+          '@svg': './assets/svg',
+          '@components': './src/components',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
