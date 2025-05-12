eea89874161ec01cbc036fab51f112b531a84bf7
shafarizkyf <shafarizkyf@gmail.com>
2025-05-12 09:54:12 +0700

    init supabase auth functions

---
+++ src/services/authentication.ts
@@ -0,0 +1,23 @@
+import supabase from './supabase';
+
+type SignInWithEmailRequest = {
+  email: string;
+  password: string;
+};
+
+export default {
+  signInWithEmail: async (data: SignInWithEmailRequest) => {
+    try {
+      return supabase.auth.signInWithPassword(data);
+    } catch (error) {
+      throw error;
+    }
+  },
+  signUpWithEmail: async (data: SignInWithEmailRequest) => {
+    try {
+      return supabase.auth.signUp(data);
+    } catch (error) {
+      throw error;
+    }
+  },
+};
